5131db76a137c42df30d8c2ac4cdd5ad95d3e1c0
范博洲 <153100629+15525730080@users.noreply.github.com>
2026-08-31 21:51:24 +0800

    Remove unused exec import and newCmd function
    Removed unused newCmd function and exec import.

--- log.go
+++ log.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 )
 
 // logInfo prints an informational message.
@@ -23,9 +22,4 @@ func logOK(format string, args ...any) {
 // logWarn prints a warning message.
 func logWarn(format string, args ...any) {
 	fmt.Printf("[WARN] "+format+"\n", args...)
-}
-
-// newCmd is a helper to create an exec.Cmd with variadic args.
-func newCmd(name string, args ...string) *exec.Cmd {
-	return exec.Command(name, args...)
 }
